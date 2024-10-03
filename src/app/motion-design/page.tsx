ac19c9652d2224294a8d45a993941498a1f197bd
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2024-10-03 17:35:25 +0100

    Added motion design page

---
+++ src/app/motion-design/page.tsx
@@ -0,0 +1,98 @@
+import Image from "next/image";
+
+import Header from "@/components/Header";
+import Introduction from "@/components/Introduction";
+import Socials from "@/components/Socials";
+
+import flyingPlaneImg from "../../../public/assets/images/flying-plane.png";
+import countdownTimerImg from "../../../public/assets/images/countdown-timer.png";
+import textAnimationImg from "../../../public/assets/images/text-animation.png";
+
+import driveIcon from "../../../public/assets/icons/drive.svg";
+
+const MotionDesignPage = () => {
+    return (
+        <div className="pt-8 mx-[5%]">
+            < Header />
+
+            <div className="mt-8 mb-24">
+                <Introduction />
+
+                <p className="text-xl leading-8 text-gray-700 max-w-[957px]">
+                    I will call myself a beginner motion designer with over a year of experience. My passion
+                    for motion design stems from the fact that I love animation. This is the reason I ventured
+                    into motion design in order to make my designs stand out.
+                </p>
+            </div>
+            <section className="mb-24">
+                <h2 className="font-extralight italic text-2xl lg:text-[2rem] mb-10">My works</h2>
+                <ul className="grid gap-16 lg:grid-cols-[repeat(auto-fill,minmax(450px,1fr))]">
+                    <li className="max-w-[550px] rounded-3xl bg-gray-300 py-8 px-5">
+                        <article className="flex flex-col items-center text-center">
+                            <div className="max-w-[340px] mb-16">
+                                <header>
+                                    <span className="inline-block bg-yellow/15 font-medium text-sm mb-4 py-1.5 px-3 rounded-2xl">Animation</span>
+                                    <h3 className="italic font-light text-2xl mb-6 leading-8">Flying Plane</h3>
+                                </header>
+                                <p className="text-gray-700 text-lg leading-8 mb-4">A simple animation of a flying plane.</p>
+                            </div>
+                            <Image
+                                src={flyingPlaneImg}
+                                alt="Flying plane"
+                            />
+                        </article>
+                    </li>
+                    <li className="max-w-[550px] border border-white bg-gray-100 rounded-3xl py-10 px-5">
+                        <article className="flex flex-col items-center text-center">
+                            <div className="max-w-[340px] mb-16">
+                                <header>
+                                    <span className="inline-block bg-yellow/15 font-medium text-sm mb-4 py-1.5 px-3 rounded-2xl">Animation</span>
+                                    <h3 className="italic font-light text-2xl mb-6 leading-8">Countdown Timer</h3>
+                                </header>
+                                <p className="text-gray-700 text-lg leading-8 mb-4">A simple clock animation timer.</p>
+                            </div>
+                            <Image
+                                src={countdownTimerImg}
+                                alt="Countdown timer"
+                            />
+                        </article>
+                    </li>
+                    <li className="max-w-[550px] border border-white bg-gray-100 rounded-3xl py-10 px-5">
+                        <article className="flex flex-col items-center text-center">
+                            <div className="max-w-[340px] mb-16">
+                                <header>
+                                    <span className="inline-block bg-yellow/15 font-medium text-sm mb-4 py-1.5 px-3 rounded-2xl">Shoppie</span>
+                                    <h3 className="italic font-light text-2xl mb-6 leading-8">Text Animation</h3>
+                                </header>
+                                <p className="text-gray-700 text-lg leading-8 mb-4">A simple text animation.</p>
+                            </div>
+                            <Image
+                                src={textAnimationImg}
+                                alt="Text animation"
+                            />
+                        </article>
+                    </li>
+                    <li className="max-w-[550px] rounded-3xl py-8 px-5 bg-gray-300">
+                        <article className="flex flex-col items-center text-center h-full">
+                            <header className="max-w-[340px] mb-16">
+                                <span className="inline-block bg-yellow/15 font-medium text-sm mb-4 py-1.5 px-3 rounded-2xl">More Work</span>
+                                <h3 className="italic font-light text-2xl mb-6 leading-8">Other Projects</h3>
+                            </header>
+                            <figure className="flex-1 grid place-items-center max-w-[300px]">
+                                <Image
+                                    src={driveIcon}
+                                    alt="Google Drive icon"
+                                />
+                                <figcaption className="text-gray-700 text-lg leading-8 mb-4">This is a Google slide presentation showing the motion I have done.</figcaption>
+                            </figure>
+                        </article>
+                    </li>
+                </ul>
+            </section>
+
+            <Socials />
+        </div>
+    )
+}
+
+export default MotionDesignPage;
